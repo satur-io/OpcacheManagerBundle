fccc41b1655972fc3466467ad29de7bb39ec0c14
Daniel Hernández <dhernandez@hiberus.com>
2021-03-08 10:26:40 +0100

    fix: fix kernel for symfony 4.4

--- src/Tests/TestApplication/src/Kernel.php
+++ src/Tests/TestApplication/src/Kernel.php
@@ -40,4 +40,14 @@ class Kernel extends BaseKernel
             (require $path)($routes->withPath($path), $this);
         }
     }
+
+    public function registerBundles(): iterable
+    {
+        $contents = require $this->getProjectDir().'/config/bundles.php';
+        foreach ($contents as $class => $envs) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+                yield new $class();
+            }
+        }
+    }
 }
